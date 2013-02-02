0a0279da0213a7dcfe190913ff9abd32d019b2ba
Jeff Gandt <jeff.gandt@gmail.com>
2013-02-02 12:50:16 -0500

    Rework the primary interface -> single method
    This reworks the primary interface to the Ostend module so that it
    maintains a single method interface.
    
    However, there are now much more control over how the module functions.
    This control is exerted by the following instance variables:
    * @ostend_attr_type -> this controls the type of attr created
        * valid values - any of :accessor, :reader, :writer
    * @ostend_attr_filter -> a whitelist array of allowed attributes
        * note that if this is set, but ostend_strict is not, Ostend will happily and **silently** discard the hash keys that are not included
    * @ostend_strict -> t/f to decide if you should throw an error when a user attempts to assign a disallowed attribute

--- lib/ostend/core.rb
+++ lib/ostend/core.rb
@@ -1,22 +1,35 @@
 module Ostend
-  def ostend_accessors( hash )
-    hash.each do |key,value|
+  def self.included(base)
+    base.send(:attr_writer, :ostend_attr_type)
+    base.send(:attr_writer, :ostend_attr_filter)
+    base.send(:attr_writer, :ostend_strict)
+  end
+
+  def ostendify( hash )
+    @ostend_attr_type ||= :accessor
+    @ostend_attr_filter ||= nil
+    @ostend_strict ||= false
+
+    ostend_raise_disallowed_attrs( hash ) if @ostend_attr_filter && @ostend_strict
+    ostend_create_attributes( hash )
+  end
+
+  def ostend_create_attributes( hash )
+    ostend_filterd_hash( hash ).each do |key,value|
       self.instance_variable_set("@#{key}", value)
-      self.class.send(:attr_accessor, key)
+      self.class.send("attr_#{@ostend_attr_type}", key)
     end
   end
 
-  def ostend_readers( hash )
-    hash.each do |key,value|
-      self.instance_variable_set("@#{key}", value)
-      self.class.send(:attr_reader, key)
-    end
+  def ostend_filterd_hash( hash )
+    return hash unless @ostend_attr_filter.respond_to?(:include?)
+    hash.keep_if{|key, val| @ostend_attr_filter.include?( key ) }
   end
 
-  def ostend_writers( hash )
-    hash.each do |key,value|
-      self.instance_variable_set("@#{key}", value)
-      self.class.send(:attr_writer, key)
-    end
+  def ostend_raise_disallowed_attrs( hash )
+    disallowed_attrs = hash.delete_if{|key, val| @ostend_attr_filter.include?( key ) }
+    return if disallowed_attrs.empty?
+    raise "The following are not allowed attributes: #{disallowed_attrs}" 
   end
+
 end
